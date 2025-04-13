352a8600d89abe8454071c93ae70dc714e1285e8
Daniel Gallups <dsgallups@protonmail.com>
2025-04-13 13:31:03 -0400

    fix: rename to iterate_voices
    Additionally, it will play them instantly everytime the timer finishes

--- bevy_midix/examples/iterate_voices.rs
+++ bevy_midix/examples/iterate_voices.rs
@@ -34,7 +34,6 @@ fn load_sf2(asset_server: Res<AssetServer>, mut synth: ResMut<Synth>) {
 
 struct VoiceChanger {
     timer: Timer,
-    note_on: bool,
     voice_number: u8,
 }
 
@@ -43,7 +42,6 @@ impl Default for VoiceChanger {
         let timer = Timer::new(Duration::from_millis(500), TimerMode::Repeating);
         VoiceChanger {
             timer,
-            note_on: true,
             voice_number: 0,
         }
     }
@@ -57,42 +55,34 @@ fn scale_me(synth: Res<Synth>, time: Res<Time>, mut scale: Local<VoiceChanger>) 
     if !scale.timer.just_finished() {
         return;
     }
-    const BASE_OCTAVE: i8 = 3;
+    const BASE_OCTAVE: i8 = 4;
     const C_CHORD: [Key; 4] = [
         Key::new(Note::C, Octave::new(BASE_OCTAVE)),
         Key::new(Note::E, Octave::new(BASE_OCTAVE)),
         Key::new(Note::G, Octave::new(BASE_OCTAVE)),
         Key::new(Note::C, Octave::new(BASE_OCTAVE + 1)),
     ];
-
-    if scale.note_on {
-        info!("Voice {}!", scale.voice_number);
-        //play a nice lil c chord
-        for key in C_CHORD {
-            synth.handle_event(ChannelVoiceMessage::new(
-                Channel::One,
-                VoiceEvent::note_on(key, Velocity::max()),
-            ));
-        }
-    } else {
-        for key in C_CHORD {
-            synth.handle_event(ChannelVoiceMessage::new(
-                Channel::One,
-                VoiceEvent::note_off(key, Velocity::max()),
-            ));
-        }
-
-        if scale.voice_number == 127 {
-            scale.voice_number = 0
-        } else {
-            scale.voice_number += 1;
-        }
+    info!("Voice {}!", scale.voice_number);
+    for key in C_CHORD {
         synth.handle_event(ChannelVoiceMessage::new(
             Channel::One,
-            // unwrapping is okay, because we don't go past 127.
-            VoiceEvent::program_change(Program::new(scale.voice_number).unwrap()),
+            VoiceEvent::note_off(key, Velocity::max()),
         ));
     }
-
-    scale.note_on = !scale.note_on;
+    if scale.voice_number == 127 {
+        scale.voice_number = 0
+    } else {
+        scale.voice_number += 1;
+    }
+    synth.handle_event(ChannelVoiceMessage::new(
+        Channel::One,
+        // unwrapping is okay, because we don't go past 127.
+        VoiceEvent::program_change(Program::new(scale.voice_number).unwrap()),
+    ));
+    for key in C_CHORD {
+        synth.handle_event(ChannelVoiceMessage::new(
+            Channel::One,
+            VoiceEvent::note_on(key, Velocity::max()),
+        ));
+    }
 }
